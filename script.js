d93ec2c1f680323f6e764d7b6fcf22f637187fa4
Mariano Goyochea <mariano.g224@gmail.com>
2026-05-31 18:35:23 -0300

    Pagina de index funcionando

--- script.js
+++ script.js
@@ -5,27 +5,65 @@ const meriendas = [
     "Jugo de naranja con ensalada de frutas", 
     "Café Americano y dos tostados",
     "Batido de vainilla con porción de torta de chocolate",
-    "Té Earl Gray y masas finas"
+    "Té Earl Grey y masas finas"
 ];
     
-
+const pizarra = document.getElementById("pizarra");
 const listaMeriendas = document.getElementById("listaMeriendas");
 
-
+let contador = 0;
 meriendas.forEach(merienda => {
     const nuevaMerienda = document.createElement("li");
     nuevaMerienda.textContent = merienda;
     listaMeriendas.appendChild(nuevaMerienda);
+    
+    contador++;
 });
 
+const botonContador = document.getElementById("botonContador");
+const contadorDeMeriendas = document.getElementById("contadorDeMeriendas");
 
-
-const seleccionado = document.getElementById("seleccionado");
-    if(cantidadSeleccion > 0){
-        pizarra.createElement("p");
-        seleccionado.textContent("Seleccionados: ", cantidadSeleccion);
-        pizarra.appendChild(seleccionado);
+botonContador.addEventListener("click", () => {
+    contadorDeMeriendas.textContent = "Cantidad de Meriendas: " + contador;
+    console.log(contador);
     }
-    else{
-        seleccionado.delete();
-    };
+)
+
+// const meriendaSeleccionada = document.querySelectorAll("#pizarra li");
+
+// meriendaSeleccionada.forEach(seleccion => {
+//     seleccion.addEventListener("click", () => {
+//         seleccion.classList.toggle("seleccionado");             usar en 2do
+//         actualizarContador();
+//     });
+// });
+
+// function actualizarContador(){
+//     const contador = document.getElementById("contador");
+//     const botonContador = document.getElementById("botonContador");
+//     const cantidadSeleccionados = document.querySelectorAll("#pizarra .seleccionado").length;
+    
+//     botonContador.addEventListener("click", () => {
+//         contador.textContent = "Seleccionados: " + cantidadSeleccionados;
+//     });
+// };
+
+
+
+
+
+// const cuenta = document.createElement("p");       
+
+// function actualizarContador(){
+//     const contadorSeleccionados = document.getElementById("contadorSeleccionados"); // espacio div
+//     const cantidadSeleccion = document.querySelectorAll("#pizarra .seleccionado").length; // cantidad de marcados dentro del codigo
+//     console.log(cantidadSeleccion); // verificacion en consola
+
+//     if(cantidadSeleccion > 0){   // si hay marcados, aparece el texto. Si no, desaparece
+//         cuenta.textContent = "Seleccionados: " + cantidadSeleccion;
+//         contadorSeleccionados.appendChild(cuenta);
+//     }
+//     else{
+//         cuenta.remove();
+//     };
+//};
